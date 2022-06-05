ffcaf69fdf50cec2ff58a8aad52a5c9c900ab3d1
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-06-04 21:25:56 -0500

    showing a new parks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewParksComponent } from './new-parks/new-parks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewParksComponent
   ],
   imports: [
     BrowserModule
